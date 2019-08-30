515e02f4d85e7c77ea56a34457e39cc242de6b12
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2019-08-30 09:29:27 -0400

    Initial Commit

---
+++ Waypoint/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Waypoint
+//
+//  Created by Edward Bennett on 8/30/19.
+//  Copyright © 2019 Edward Bennett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
